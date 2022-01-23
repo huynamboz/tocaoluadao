dca4f0e0d0cf3e233161559195e565f0e7a9a6ef
tuanduong1907 <91052899+tuanduong1907@users.noreply.github.com>
2022-01-23 11:20:49 +0700

    Update prestigious-service.js

--- assets/js/prestigious-service.js
+++ assets/js/prestigious-service.js
@@ -1,4 +1,6 @@
 const reputationHeader = document.querySelectorAll('.reputation-item__header')
+const searchInput = document.querySelector('#search-input')
+const serachForm = document.querySelector('.form-search')
 // Function
 function showReputationBody(e) {
     const reputationBody = e.currentTarget.nextElementSibling
@@ -13,3 +15,14 @@ function showReputationBody(e) {
 
 // Event
 reputationHeader.forEach(item => item.addEventListener('click', showReputationBody))
+searchInput.addEventListener('click', (e) => {
+    e.stopPropagation();
+    serachForm.classList.add('active');
+})
+
+document.body.addEventListener('click', (e) => {
+    const active = document.querySelector('.active');
+    if(active) {
+        serachForm.classList.remove('active');
+    }
+})
